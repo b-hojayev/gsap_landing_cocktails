48b51e2e74a9a7d71e46b95978a55b6cbd4b355a
b-hojayev <bgnchojayew@gmail.com>
2025-07-18 22:11:05 +0500

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="text-3xl text-indigo-300 flex-center h-[100vh]">App</div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
